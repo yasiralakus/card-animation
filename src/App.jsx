c531497b3a9a72e05c2f33402e74e2caa46c4233
Yasir Alakus <yasiralakus11@gmail.com>
2024-04-04 22:51:31 +0300

    stil 01 eklendi

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+export default function App() {
+
+    return (
+        <div className="full-page">
+
+            <div className="container">
+
+                <div className="card">
+
+                    <i class="fa-solid fa-palette"></i>
+
+                    <h1>Design & Art</h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, deleniti! Porro, perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                </div>
+
+                <div className="card">
+
+                    <i class="fa-solid fa-building-columns"></i>
+
+                    <h1>Accountance</h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, deleniti! Porro, perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                </div>
+
+                <div className="card">
+
+                    <i class="fa-solid fa-lightbulb"></i>
+
+                    <h1>Management</h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, deleniti! Porro, perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
