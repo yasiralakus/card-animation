bc70c8b2ab59b066d41a393843411a5cd581fdc6
Yasir Alakus <yasiralakus11@gmail.com>
2024-04-07 00:38:19 +0300

    stil 07, stil 08 eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -145,23 +145,7 @@ export default function App() {
                     </div>
 
                     <div>
-                        <img src="https://images.unsplash.com/photo-1712371963733-7c7a454f6973?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <div>
-                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, sequi!</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur provident excepturi maiores eveniet iusto quia temporibus, sed suscipit facilis, eius, tenetur necessitatibus similique recusandae sint consectetur corrupti quis nostrum nam fugit itaque illum et!</p>
-                        </div>
-                    </div>
-
-                    <div>
                         <img src="https://images.unsplash.com/photo-1711972964921-17046ddf8d12?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <div>
-                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, sequi!</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur provident excepturi maiores eveniet iusto quia temporibus, sed suscipit facilis, eius, tenetur necessitatibus similique recusandae sint consectetur corrupti quis nostrum nam fugit itaque illum et!</p>
-                        </div>
-                    </div>
-
-                    <div>
-                        <img src="https://images.unsplash.com/photo-1712339793959-f4787f04c6c0?q=80&w=2005&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                         <div>
                             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, sequi!</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur provident excepturi maiores eveniet iusto quia temporibus, sed suscipit facilis, eius, tenetur necessitatibus similique recusandae sint consectetur corrupti quis nostrum nam fugit itaque illum et!</p>
@@ -174,6 +158,86 @@ export default function App() {
                             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, sequi!</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur provident excepturi maiores eveniet iusto quia temporibus, sed suscipit facilis, eius, tenetur necessitatibus similique recusandae sint consectetur corrupti quis nostrum nam fugit itaque illum et!</p>
                         </div>
+                    </div>
+
+                </div>
+
+                <div className="animation-07">
+
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1712311953600-81354526ec28?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    </div>
+
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1711972964921-17046ddf8d12?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    </div>
+
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1712313171623-5df9435ec12b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    </div>
+
+                </div>
+
+                <div className="animation-08">
+
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1712260559341-cbe284e828b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                        <h6>Music</h6>
+
+                        <h5>12 Jun 2024</h5>
+
+                        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptatem id totam dolores illum error veniam placeat inventore debitis maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at officia aliquid aliquam sit suscipit!</p>
+
+                        <div>
+                            <p><i class="fa-solid fa-heart"></i> 8</p>
+                            <p><i class="fa-solid fa-comment"></i> 14</p>
+                        </div>
+
+                        <button><i class="fa-solid fa-right-long"></i> READ MORE</button>
+
+                    </div>
+
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1712329113006-97e9c76835f5?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                        <h6>Adventure</h6>
+
+                        <h5>23 Apr 2024</h5>
+
+                        <h1>Id totam dolores illum error veniam placeat inventore.</h1>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptatem id totam dolores illum error veniam placeat inventore debitis maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at officia aliquid aliquam sit suscipit!</p>
+
+                        <div>
+                            <p><i class="fa-solid fa-heart"></i> 11</p>
+                            <p><i class="fa-solid fa-comment"></i> 18</p>
+                        </div>
+
+                        <button><i class="fa-solid fa-right-long"></i> READ MORE</button>
+
+                    </div>
+
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1712238501880-2e4efd062211?q=80&w=2056&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                        <h6>Travel</h6>
+
+                        <h5>10 Nov 2024</h5>
+
+                        <h1>Totam dolores illum veniam placeat inventore debitis maxime?</h1>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptatem id totam dolores illum error veniam placeat inventore debitis maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at officia aliquid aliquam sit suscipit!</p>
+
+                        <div>
+                            <p><i class="fa-solid fa-heart"></i> 3</p>
+                            <p><i class="fa-solid fa-comment"></i> 2</p>
+                        </div>
+
+                        <button><i class="fa-solid fa-right-long"></i> READ MORE</button>
+
                     </div>
 
                 </div>
